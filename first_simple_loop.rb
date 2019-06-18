e04bdaceba74f8851629715d21949d02dd9003c6
dmynarski <dominik.mynarski@onet.pl>
2019-06-18 23:59:26 +0200

    Matching lottery numbers. Integrated with client

--- first_simple_loop.rb
+++ first_simple_loop.rb
@@ -1,7 +1,15 @@
+#Lotto lottery match finder
 lottery = Array.new(6) { rand(1..100)}
-wins = Array.new(6) { rand(1..100)}
-times = 0
-until lottery == wins
-  puts "Oops, you didn't win in lottery, we try again. It's attempt number : #{times}"
-  times += 1
+puts "Type 6 numbers from 1 to 100, separated by commas to check matching lottery numbers"
+nums = gets.chomp.split(",").map {|n| n.to_i}
+nums.slice(0)
+until nums.length < 6
+  nums.pop
+end
+matches = nums.select { |n| lottery.include?(n)}
+if !matches.empty?
+  puts "Congrats! You have #{matches.length} matches!"
+puts "Your numbers are : #{nums}, lottery numbers are #{lottery} \nMatching numbers are : #{matches}"
+else
+  puts "Im sorry, your numbers doesn't match to lottery numbers. \nLottery numbers are : #{lottery}"
 end
