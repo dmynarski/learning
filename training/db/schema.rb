271fc1ff67ea24d476da0177bf75fec604051108
dmynarski <dominik.mynarski@onet.pl>
2019-07-03 20:58:15 +0200

    Associations

--- training/db/schema.rb
+++ training/db/schema.rb
@@ -10,11 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_20_135030) do
+ActiveRecord::Schema.define(version: 2019_07_03_181734) do
 
   create_table "todos", force: :cascade do |t|
     t.string "name"
     t.text "description"
+    t.integer "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
